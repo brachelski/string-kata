d588b4dd516d52068b21411d6df8b3c97c75edc8
brandon <brandon@rachelski.net>
2022-05-09 08:10:22 -0500

    init

---
+++ src/main/java/com/brandonrachelski/stringkata/StringKataApplication.java
@@ -0,0 +1,13 @@
+package com.brandonrachelski.stringkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brandonrachelski/stringkata/StringKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brandonrachelski.stringkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
